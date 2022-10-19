3349165feb46d1ea0f12db926b7b396229a36d98
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-10-19 14:22:50 -0600

    wait for check

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 // IPO Algorithm (Input-Process-Output)
 
 // Input
+alert("Welcome to Pizzas For Order.")
+alert("We offer every kind of pizza with one topping of any sort, Please order whatever you'd like.")
 let pizza = prompt(
     "What pizza would you like? ");
 let toppings = prompt("What toppings would you like to add?");
