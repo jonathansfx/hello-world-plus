1d6cb7dce849b1ce415a54ed70dc37b797c3ad24
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-10-19 14:06:52 -0600

    sag;jknb

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// JavaScript Code
+// HELLO WORLD PLUS by Jonathan
+
+// IPO Algorithm (Input-Process-Output)
+
+// Input
+let pizza = prompt(
+    "What pizza would you like? ");
+let toppings = prompt("What toppings would you like to add?");
+        
+// Process
+let msg = `Your ${pizza} Pizza with ${toppings} will be ready in 20 minutes.`;
+
+// Output
+alert(msg);
