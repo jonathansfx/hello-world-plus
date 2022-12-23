b1f7fcea257d8e5dc57af87953c9219a7ae76f54
jonathansfx <jonathanluo22@gmail.com>
2022-12-23 12:12:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,13 @@
 // IPO Algorithm (Input-Process-Output)
 
 // Input
-alert("Welcome to Pizzas For Order.")
-alert("We offer every kind of pizza with one topping of any sort, Please order whatever you'd like.")
-let pizza = prompt(
-    "What pizza would you like? ");
+alert("Welcome to Pizzas For Order.");
+alert(
+  "We offer every kind of pizza with one topping of any sort, Please order whatever you'd like."
+);
+let pizza = prompt("What pizza would you like? ");
 let toppings = prompt("What toppings would you like to add?");
-        
+
 // Process
 let msg = `Your ${pizza} Pizza with ${toppings} will be ready in 20 minutes.`;
 
